ff98dcb611d29a0426585e6f5afe494def627ef1
Theofilos Chamalis <t.chamalis@vieconnect.io>
2020-04-25 21:43:42 +0300

    Added grid for the card list to make it responsive

--- src/components/TweetList.tsx
+++ src/components/TweetList.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { IonList, IonListHeader, IonLabel, IonButton } from '@ionic/react';
+import { IonList, IonListHeader, IonLabel, IonButton, IonGrid, IonRow, IonCol } from '@ionic/react';
 import axios from 'axios';
 import TweetCard from './TweetCard';
 
@@ -29,40 +29,46 @@ export default class TweetList extends Component<{}, { loremIpsumTexts: Array<st
 
     render() {
         return (
-            <IonList className="ion-padding">
-                <IonListHeader lines="none">
-                    <IonLabel>New tweets</IonLabel>
-                    <IonButton>See All</IonButton>
-                </IonListHeader>
-                <TweetCard
-                    cardTitle="John Travolta"
-                    cardSubtitle="Fisherman"
-                    cardBodyImage="none"
-                    cardBodyText="This is my first tweet! Please do not reply to me before 15:30 since I will be on the lake fishing :("
-                    cardTimestamp="Sent at 09:30"
-                />
-                <TweetCard
-                    cardTitle="Jimmy Falon"
-                    cardSubtitle="none"
-                    cardBodyImage="https://picsum.photos/400"
-                    cardBodyText="This is the first photo I have taken !!!"
-                    cardTimestamp="Sent at 14:19"
-                />
-                <TweetCard
-                    cardTitle="Gerard Du Pardieu"
-                    cardSubtitle="Ruski"
-                    cardBodyImage="https://picsum.photos/420"
-                    cardBodyText={this.state.loremIpsumTexts.length > 0 ? this.state.loremIpsumTexts[0] : "Je ne pais patisserie"}
-                    cardTimestamp="Sent at 16:38"
-                />
-                <TweetCard
-                    cardTitle="Kastorikos"
-                    cardSubtitle="Chef"
-                    cardBodyImage="https://picsum.photos/410"
-                    cardBodyText={this.state.loremIpsumTexts.length > 0 ? this.state.loremIpsumTexts[1] : "Oui bebe oui"}
-                    cardTimestamp="Sent at 16:51"
-                />
-            </IonList>
+            <IonGrid className="ion-no-padding">
+                <IonRow>
+                    <IonCol sizeMd="8" offsetMd="2" sizeLg="6" offsetLg="3">
+                        <IonList className="ion-padding">
+                            <IonListHeader lines="none">
+                                <IonLabel>New tweets</IonLabel>
+                                <IonButton>See All</IonButton>
+                            </IonListHeader>
+                            <TweetCard
+                                cardTitle="John Travolta"
+                                cardSubtitle="Fisherman"
+                                cardBodyImage="none"
+                                cardBodyText="This is my first tweet! Please do not reply to me before 15:30 since I will be on the lake fishing :("
+                                cardTimestamp="Sent at 09:30"
+                            />
+                            <TweetCard
+                                cardTitle="Jimmy Falon"
+                                cardSubtitle="none"
+                                cardBodyImage="https://picsum.photos/600"
+                                cardBodyText="This is the first photo I have taken !!!"
+                                cardTimestamp="Sent at 14:19"
+                            />
+                            <TweetCard
+                                cardTitle="Gerard Du Pardieu"
+                                cardSubtitle="Ruski"
+                                cardBodyImage="https://picsum.photos/620"
+                                cardBodyText={this.state.loremIpsumTexts.length > 0 ? this.state.loremIpsumTexts[0] : "Je ne pais patisserie"}
+                                cardTimestamp="Sent at 16:38"
+                            />
+                            <TweetCard
+                                cardTitle="Kastorikos"
+                                cardSubtitle="Chef"
+                                cardBodyImage="https://picsum.photos/610"
+                                cardBodyText={this.state.loremIpsumTexts.length > 0 ? this.state.loremIpsumTexts[1] : "Oui bebe oui"}
+                                cardTimestamp="Sent at 16:51"
+                            />
+                        </IonList>
+                    </IonCol>
+                </IonRow>
+            </IonGrid>
         );
     }
 }
