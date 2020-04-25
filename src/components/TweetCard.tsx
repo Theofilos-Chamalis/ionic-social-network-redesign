fd4458fc9d27b479a49c9d50e8c6f655d01d490c
Theofilos Chamalis <t.chamalis@vieconnect.io>
2020-04-25 21:00:37 +0300

    Made the TweetCard content modular with an IonImg component

--- src/components/TweetCard.tsx
+++ src/components/TweetCard.tsx
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
-import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonText, IonLabel } from '@ionic/react';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonText, IonLabel, IonImg } from '@ionic/react';
+import './TweetCard.css'
 
-export default class TweetCard extends Component<{ cardTitle: string, cardSubtitle: string, cardBody: string, cardTimestamp: string }, {}> {
+export default class TweetCard extends Component<{ cardTitle: string, cardSubtitle: string | 'none', cardBodyImage: string | 'none', cardBodyText: string | 'none', cardTimestamp: string | 'none' }, {}> {
     constructor(props: any) {
         super(props)
 
         this.state = {}
     }
 
+    isNone = (propToCheck: string) => {
+        if (propToCheck === 'none') {
+            return null;
+        }
+
+        return propToCheck;
+    }
+
     render() {
         return (
-            <IonCard>
+            <IonCard mode="ios">
                 <IonCardHeader>
-                    <IonCardTitle>{this.props.cardTitle}</IonCardTitle>
-                    <IonCardSubtitle>{this.props.cardSubtitle}</IonCardSubtitle>
+                    <IonCardTitle>{this.isNone(this.props.cardTitle)}</IonCardTitle>
+                    <IonCardSubtitle>{this.isNone(this.props.cardSubtitle)}</IonCardSubtitle>
                 </IonCardHeader>
                 <IonCardContent>
-                    <IonText>{this.props.cardBody}</IonText>
+                    {this.isNone(this.props.cardBodyImage) ?
+                        <IonImg src={this.props.cardBodyImage} className="ion-margin-bottom" /> :
+                        null
+                    }
+                    <IonText>{this.isNone(this.props.cardBodyText)}</IonText>
                     <IonLabel className="ion-text-right">
-                        <p>{this.props.cardTimestamp}</p>
+                        <p><br />{this.isNone(this.props.cardTimestamp)}</p>
                     </IonLabel>
                 </IonCardContent>
             </IonCard>
--- src/components/TweetList.tsx
+++ src/components/TweetList.tsx
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import { IonList, IonListHeader, IonLabel, IonButton } from '@ionic/react';
 import TweetCard from './TweetCard';
 
-export default class TweetList extends Component<{}, {}> {
+export default class TweetList extends Component<{}, { loremIpsumTexts: Array<string>, loremIpsumTextNumber: number }> {
     constructor(props: any) {
         super(props)
 
-        this.state = {}
+        this.state = {
+            loremIpsumTexts: [],
+            loremIpsumTextNumber: 2
+        }
     }
+
+    componentDidMount = () => {
+        for (let i = 0; i < this.state.loremIpsumTextNumber; i++) {
+            this.generateLoremIpsum();
+        }
+    };
+
+    generateLoremIpsum = () => {
+        const loremText = 'random text lalala';
+        this.state.loremIpsumTexts.push(loremText);
+    };
 
     render() {
         return (
@@ -19,7 +33,15 @@ export default class TweetList extends Component<{}, {}> {
                 <TweetCard
                     cardTitle="John Travolta"
                     cardSubtitle="Fisherman"
-                    cardBody="This is my first tweet! Please do not reply to me before 15:30 since I will be on the lake fishing :("
+                    cardBodyImage="none"
+                    cardBodyText="This is my first tweet! Please do not reply to me before 15:30 since I will be on the lake fishing :("
+                    cardTimestamp="Sent at 09:30"
+                />
+                <TweetCard
+                    cardTitle="Jimmy Falon"
+                    cardSubtitle="none"
+                    cardBodyImage="https://picsum.photos/400"
+                    cardBodyText="This is the first photo I have taken !!!"
                     cardTimestamp="Sent at 09:30"
                 />
             </IonList>
